6abba34cf4078254048e18b3e09330347ddd1963
FHSWar <17017307919@163.com>
2020-11-25 17:16:01 +0800

    做首页顶栏

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,35 @@
 <template>
   <div id="app">
-
+    <Header></Header>
   </div>
 </template>
-
-<style lang="scss">
-
+<script>
+import Header from './components/Player/PlayerHeader'
+export default {
+  name: 'App',
+  components: {
+    Header
+  }
+}
+</script>
+<style scoped lang="scss">
+@import "assets/css/variable";
+.theme{
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  button{
+    width: 100px;
+    height: 100px;
+    &:nth-of-type(1){
+      background: $bgc-theme;
+    }
+    &:nth-of-type(2){
+      background: $bgc-theme1;
+    }
+    &:nth-of-type(3){
+      background: $bgc-theme2;
+    }
+  }
+}
 </style>
